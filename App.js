dd30ede5824bb3ceb221602eafa6c981f0adedf0
Hossein <djalicrt@gmail.com>
2020-12-20 14:03:08 +0330

    3rd Part :: Complete Weather And Info Box with Style
    Fetch WebService 
    Show Data From WebService

--- App.js
+++ App.js
@@ -20,8 +20,34 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: '',
-    };
+      city: "",
+      data:[],
+      icon:"",
+      city_display:"",
+      desc:"",
+      main:"",
+      humidity:"",
+      pressure:"",
+      visibility:""
+    }
+    this.fetch_weather()
+  }
+  //Arrow Function
+  fetch_weather=()=>{
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=ahvaz&units=metric&appid=5a873b6b606d9e2aa79aff040a0e7d63")
+    .then((response) => response.json())
+    .then((json => {
+      this.setState({data:json})
+      this.setState({ temp : json.main.temp+" °C"})
+      this.setState({city_display : json.name})
+      this.setState({ desc : json.weather[0].description})
+      this.setState({ icon : json.weather[0].icon})
+      this.setState({ main : json.weather[0].main})
+      this.setState({ humidity : json.main.humidity+ " %"})
+      this.setState({ pressure : json.main.pressure+ " hPa"})
+      this.setState({ visibility : (json.main.visibility/1000).toFixed(2)+ " Km"})
+    })).catch((error)=>console.error(error))
+   
   }
 
   render() {
@@ -46,8 +72,25 @@ export default class App extends React.Component {
           </View>
 
           <View style={styles.weather_Box_Main}>
-            <View style={styles.weather_Holder_View}></View>
+            <View style={styles.weather_Holder_View}>
+              <Image source={{ uri:"https://openweathermap.org/img/wn/"+this.state.icon+"@2x.png" }} style={styles.weather_Image} />
+              <View>
+                <Text style={styles.temp_text}> {this.state.temp} </Text>
+                <Text style={styles.city_text}> {this.state.city_display} </Text>
+              </View>
+            </View>
           </View>
+
+          <View style={styles.info_Box_View}>
+            <View style={styles.info_Holder_View}>
+                 <Text style={styles.Main_Wether_Text}> {this.state.main} </Text>
+                 <Text style={styles.desc_Text}> {this.state.desc} </Text>
+                 <Text style={styles.humidity_Text}> {this.state.humidity} </Text>
+                 <Text style={styles.other_text}> {this.state.pressure} </Text>
+                  <Text style={styles.other_text}> {this.state.visibility} </Text>
+         </View>
+         </View>
+
         </ImageBackground>
       </SafeAreaView>
     );
@@ -96,9 +139,62 @@ const styles = StyleSheet.create({
   weather_Holder_View: {
     height:"80%",
     width:"90%",
-    backgroundColor:"rgba(255,255,255,0.3",
+    backgroundColor:"rgba(255,255,255,0.3)",
     borderRadius:15,
     alignItems:"center",
     flexDirection:"row"
+  },
+  weather_Image: {
+    height: "80%",
+    width:"50%"
+  },
+  temp_text:{
+    fontSize:30,
+    color: "#FFF",
+    marginLeft:"5%"
+  },
+  city_text:{
+    fontSize:20,
+    color:"#FFF",
+    marginLeft:"5%",
+    marginTop: "3%"
+  },
+  info_Holder_View:{
+    height:"80%",
+    width:"90%",
+    backgroundColor: "rgba(255,255,255,0.92)",
+    borderRadius:15
+  },
+  info_Box_View:{
+    height:"45%",
+    width:"100%",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  Main_Wether_Text:{
+    fontSize:28,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"8%"
+  },
+  desc_Text : {
+    fontSize:20,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"5%"
+  },
+   humidity_Text : {
+    fontSize:18,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"5%"
+  },
+  other_text : {
+    fontSize:18,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"2%"
   }
+
+
 });
